6461076bb2c65f8fa67c2b6d31a82b6e860d445b
Silas Mahler <silas-mahler@t-online.de>
2020-03-21 15:29:16 +0100

    WIP: add general news idea

---
+++ frontend/src/app/tab3/news.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {INews} from "./news";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  private _url: string ="http://newsapi.org/v2/top-headlines?country=de&category=health&apiKey=4d6381250b7243df9fa889a380113343"
+
+  constructor(private http: HttpClient) { }
+
+  getNews(): Observable<INews[]> {
+    return this.http.get<INews[]>(this._url);
+  }
+}
---
+++ frontend/src/app/tab3/news.ts
@@ -0,0 +1,10 @@
+export interface INews {
+        sourceName: string,
+        author: string,
+        title: string,
+        description: string,
+        url: string,
+        urlToImage: string,
+        publishedAt: string,
+        content: string
+}
