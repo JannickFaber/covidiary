eec4709a6a328bab26e8bc3e8b9253663020f48b
Johannes Teklote <johannes.teklote@web.de>
2020-03-21 01:48:15 +0100

    add backend

---
+++ backend/src/main/java/de/urbanrednecks/covidiary/CovidiaryApplication.java
@@ -0,0 +1,11 @@
+package de.urbanrednecks.covidiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidiaryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CovidiaryApplication.class, args);
+    }
+}
