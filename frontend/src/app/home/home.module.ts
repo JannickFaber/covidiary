61127757284a4ff1e0edd91f1b0c1fd664b8d6e3
splitt3r <splitt3r@users.noreply.github.com>
2020-03-22 12:28:05 +0100

    remove calendar module

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -4,17 +4,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-import { NgCalendarModule } from 'ionic2-calendar';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    RouterModule.forChild([{ path: '', component: HomePage }]),
-    NgCalendarModule
+    RouterModule.forChild([{ path: '', component: HomePage }])
   ],
   declarations: [HomePage]
 })
